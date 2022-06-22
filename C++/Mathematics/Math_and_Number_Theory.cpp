b305f34f28a9bc5d74fd51d2afc9c9fd681f6de4
Shivam-9212 <Ps450651@gmail.com>
2022-06-22 22:59:03 +0530

    Day 14

--- C++/Mathematics/Math_and_Number_Theory.cpp
+++ C++/Mathematics/Math_and_Number_Theory.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<math.h>
 using namespace std;
 int no_of_trailing_zerores_in_factorial(int a){
     int result = 0;
@@ -6,6 +8,22 @@ int no_of_trailing_zerores_in_factorial(int a){
         result = result + (a/i);
     }
     return result;
+}
+bool prime_numbers(int a){
+    vector<int> arr;
+    for(int i = 2; i*i <= a; i++){
+        if((a % i) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+void total_prime_factors(int a){
+    vector<int> vec1;
+
+    for(int i = 0; i < vec1.size(); i++){
+        cout<<vec1.at(i)<<" ";
+    }
 }
 void palindrome(int a){
     int c = a;
@@ -23,6 +41,6 @@ void palindrome(int a){
     }
 }
 int main(){
-    palindrome(1919);
+    total_prime_factors(18);
     return 0;
 }
